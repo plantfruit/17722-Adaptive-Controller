4b6ab8c4cefa7d0800605dc6bf2fc494a36cfdc6
Anu <anusitaraman@gmail.com>
2025-04-28 15:16:16 -0400

    added some more code that may be useful

--- FFTPointRegression.py
+++ FFTPointRegression.py
@@ -62,38 +62,58 @@ train_indices = []
 test_indices = []
 total_files = num_labels * files_per_label
 
-# Read features and labels from file
-X = np.loadtxt(fileName)
-#print(np.shape(X))
-if (stringLabel):
-    y = np.loadtxt(labelFileName, dtype = str)
-elif (floatLabel):
-    y = np.loadtxt(labelFileName) * 10
-else:
-    y = np.loadtxt(labelFileName)
+def get_feats_from_file(fileName):
+    # Read features and labels from file
+    X = np.loadtxt(fileName)
+    #print(np.shape(X))
+    if (stringLabel):
+        y = np.loadtxt(labelFileName, dtype = str)
+    elif (floatLabel):
+        y = np.loadtxt(labelFileName) * 10
+    else:
+        y = np.loadtxt(labelFileName)
 
-# Reshape 1 column/1 row files 
-if X.ndim == 1:
-    X_reshaped = X.reshape(-1, 1)
-else:
-    X_reshaped = X
+    # Reshape 1 column/1 row files 
+    if X.ndim == 1:
+        X_reshaped = X.reshape(-1, 1)
+    else:
+        X_reshaped = X
 
-df = pd.DataFrame(X)
-df['label'] = y
+    df = pd.DataFrame(X)
+    df['label'] = y
+    df['press_no_press_label'] = np.where(df['label'] == 6, 0, 1)  # Assuming label 6 is "no press"
 
-use_df = df[df['label'] != 6]
-train_press_no_press, test_press_no_press = train_test_split(df)
+    #getting train_test_split for the press no press model
+    press_no_press_df = df.drop(['press_no_press_label'], axis=1)
+    train_press_no_press, test_press_no_press = train_test_split(press_no_press_df)
 
-train, test = train_test_split(use_df)
+    #all training data for press_no_press classifier model. Train the final model on this data
+    all_press_no_press_x = press_no_press_df.drop(['press_no_press_label'], axis=1).to_numpy()
+    all_press_no_press_y = press_no_press_df['press_no_press_label'].to_numpy()
 
-all_x = use_df.drop(['label'], axis=1).to_numpy()
-all_y = use_df['label'].to_numpy()
-train_x = train.drop(['label'], axis=1).to_numpy()
-train_y = train['label'].to_numpy()
-test_x = test.drop(['label'], axis=1).to_numpy()
-test_y = test['label'].to_numpy()
+    #training and testing split for press_no_press classifier model. Use for checking accuracy of the model. 
+    train_press_no_press_x = train_press_no_press.drop(['press_no_press_label'], axis=1).to_numpy()
+    train_press_no_press_y = train_press_no_press['press_no_press_label'].to_numpy()
+    test_press_no_press_x = test_press_no_press.drop(['press_no_press_label'], axis=1).to_numpy()
+    test_press_no_press_y = test_press_no_press['press_no_press_label'].to_numpy()
 
+    #getting train_test_split for the y-axis regression model
+    #note: change this if we are no longer using 6 as the label for the "no press" state
+    use_df = df[df['label'] != 6]
+    train, test = train_test_split(use_df)
 
+    #all training data for y-axis regression model. Train the final model on this data
+    all_x = use_df.drop(['label'], axis=1).to_numpy()
+    all_y = use_df['label'].to_numpy()
+
+    #training and testing split for y-axis regression model. Use for checking accuracy of the model. 
+    train_x = train.drop(['label'], axis=1).to_numpy()
+    train_y = train['label'].to_numpy()
+    test_x = test.drop(['label'], axis=1).to_numpy()
+    test_y = test['label'].to_numpy()
+    return all_x, all_y, train_x, train_y, test_x, test_y, all_press_no_press_x , all_press_no_press_y, train_press_no_press_x, train_press_no_press_y, test_press_no_press_x, test_press_no_press_y
+
+all_x, all_y, train_x, train_y, test_x, test_y, all_press_no_press_x , all_press_no_press_y, train_press_no_press_x, train_press_no_press_y, test_press_no_press_x, test_press_no_press_y = get_feats_from_file(fileName)
 '''if (not(kFoldOrNot)):
     for label in range(1, num_labels + 1):
         # Get all rows for this label
@@ -131,6 +151,8 @@ test_y = test['label'].to_numpy()
 #model =  MLPRegressor(random_state=1, max_iter=2000, tol=0.1)
 model_x = SVR()
 model_y = SVR()
+
+#same model as kevin's initial model
 model_press_no_press = SVC(kernel='linear')
 
 #from left to center 
@@ -174,7 +196,8 @@ def relabel_data(y):
     return np.array(new_x_axis_y).reshape(-1, 1), np.array(new_y_axis_y).reshape(-1, 1), np.array(y_no_press).reshape(-1,1)
 '''
 
-def relabel_data_x(data, no_press):
+#TODO: fix this relabeling function such that it works with the labels given in the datafiles. 
+def relabel_data_y(x, y):
     no_press_x = []
     no_press_y = []
     y = []
@@ -189,7 +212,7 @@ test_x, test_y, test_no_press_x, test_no_press_y = relabel_data_x(test_x, test_y
 
 model_y.fit(train_x, train_y)
 #model_y.fit(train_x, train_y_axis_y)
-model_press_no_press.fit(train_press_no_press, train_no_press_y)
+model_press_no_press.fit(train_press_no_press_x, train_no_press_y)
 
 # Make predictions on the test set
 #y_pred_x = model_x.predict(test_x)
@@ -201,6 +224,34 @@ mse_y = mean_squared_error(test_y, y_pred_y)
 
 print(mse_y)
 
+def display_mse(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    print(f"Mean Squared Error: {mse:.2f}")
+
+    # Plotting the results
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=y_true, y=y_pred)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    plt.title('True Values vs Predictions')
+    plt.plot([y_true.min(), y_true.max()], [y_true.min(), y_true.max()], 'r--')  # Diagonal line
+    plt.grid()
+    plt.show()
+
+def final_train_and_save_model(model, X, y, model_name):
+    if (convertModel):
+        # Code taken from documentation
+        # Convert scikit-learn model to ONNX format
+        onnx_model = to_onnx(model, X.astype(np.float32), target_opset = 12)
+        #ONNX verification
+        #y_pred = predict_with_onnxruntime(onnx_model, X_test)
+        #accuracy = accuracy_score(y_test, y_pred)
+        #print(accuracy)
+
+        # Export to ONNX
+        #initial_type = [('input', FloatTensorType([None, 150]))]  # match your vector length
+        #onnx_model = convert_sklearn(model, initial_types=initial_type)
+
 def predict_with_onnxruntime(onx, X):
     sess = InferenceSession(onx.SerializeToString(), providers=["CPUExecutionProvider"])
     input_name = sess.get_inputs()[0].name
@@ -208,18 +259,7 @@ def predict_with_onnxruntime(onx, X):
     return res[0]
 
 
-if (convertModel):
-    # Code taken from documentation
-    # Convert scikit-learn model to ONNX format
-    onnx_model = to_onnx(model, X.astype(np.float32), target_opset = 12)
-    #ONNX verification
-    y_pred = predict_with_onnxruntime(onnx_model, X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(accuracy)
 
-    # Export to ONNX
-    #initial_type = [('input', FloatTensorType([None, 150]))]  # match your vector length
-    #onnx_model = convert_sklearn(model, initial_types=initial_type)
 
 
 '''
