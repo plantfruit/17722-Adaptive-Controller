6577866cfc15fa3eef4b3d5764c0a99324436116
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2025-04-29 15:03:46 -0400

    Depth Level Data
    Collected by Yifeng

--- LabelCreator.m
+++ LabelCreator.m
@@ -2,7 +2,7 @@
 
 pulseNum = 1; % Number of pulses extracted from each file
 fileNum = 10; % Number of trials conducted for each label
-gridSize = 8; % Number of labels, or points in the grid. E.g. If it's a 5x5 grid, we have 25 points
+gridSize = 4; % Number of labels, or points in the grid. E.g. If it's a 5x5 grid, we have 25 points
 
 labels = zeros(gridSize * fileNum * pulseNum, 1);
 %labels = strings(gridSize * fileNum * pulseNum, 1);
@@ -15,4 +15,4 @@ for i = 1:gridSize
     end
 end
 
-save("linreg_1_labels.txt", "labels", "-ascii")
+save("depth_1_labels.txt", "labels", "-ascii")
